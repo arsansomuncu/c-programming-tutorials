c2aedb2264a481d231c5c7a7810a74bc2f79f6be
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-24 14:45:27 +0300

    Create type conversion.cpp

---
+++ type conversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+  double x = 3.14;
+  std:: << cout (int) x << '\n';
+
+  int correct = 8;
+  int questions = 10; 
+  double score = correct / double (questions) * 100;
+  std:: << cout << score << "%";
+
+  return 0;
+}
