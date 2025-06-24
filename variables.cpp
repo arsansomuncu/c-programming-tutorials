0df8da480c0b9ea9941eaaa37ef6789674e628a0
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-24 14:12:38 +0300

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(){
+  int age = 21;
+  int year = 2023;
+  int days = 365;
+
+  double price = 10.99;
+  double gpa = 2.5;
+  double temperature = 20.1; 
+
+  char grade = 'a';
+  char initial = 'b';
+  char currency = '$';
+
+  bool student = true;
+  bool power = false;
+  bool forSale = false;
+
+  std::string name = "bro";
+  std::string age = "21";
+  std::string food = "pizza";
+  std::string address = "fake st. 123";
+
+  std:: cout << "hello" << name;
+  std:: cout << "you are" << age << "years old";
+
+  return 0;
+}
