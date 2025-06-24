edce556f7f4efb2c808c57ed84693403254188c6
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-24 14:49:58 +0300

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include <<iostream>
+#include <vector>
+
+typedef std::string text_t;
+typedef int number_t;
+
+using text_t = std::string
+using number_t = int 
+
+int main(){
+text_t firstName = "bob";
+std:: cout << firstName << '\n';
+
+number_t age = 21;
+std:: cout << age << '\n';
+
+return 0;
+}
