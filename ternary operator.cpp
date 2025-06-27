fb1da51606845ccb7299238e65c9a02691951b00
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-27 11:22:39 +0300

    Create ternary operator.cpp

---
+++ ternary operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+ int grade = 50;
+ grade >= 60 ? std:: cout << "you pass" : std:: cout << "you fail";
+ 
+ // int number = 9;
+ // number % 2 == 1 ? std:: cout << "odd" : std:: cout << "even";
+  
+// bool hungry = true;
+// hungry ? std:: cout << "you are hungry" : std:: cout << "you are full";
+
+return 0;
+}
