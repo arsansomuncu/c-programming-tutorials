a8c0ad8ce91dfcc322a69e5780eb54100b7c3229
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-27 11:42:45 +0300

    Create logical operators.cpp

---
+++ logical operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(){
+  bool sunny;
+
+  int temp;
+  std:: cout << "enter the temperature";
+  std:: cin >> temp;
+
+  if(0 <= temp || temp >= 30){
+    std:: cout << "the temperature is bad";
+  }
+  else{
+    std:: cout << "the temperature is good";
+  }
+  if (sunny){
+    std:: cout << "it is sunny outside";
+  }
+  else{
+    std:: cout << "it is cloudy outside";
+  }
+  // if(temp > 0 && temp < 30){
+   // std:: cout << "the temperature is good";
+  // }
+  // else {
+    // std:: cout << "the temperature is bad";
+  //}
+  return 0;
+}
