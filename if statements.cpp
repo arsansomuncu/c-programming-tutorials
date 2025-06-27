236b51b0c9bf541d4b02b82494491bd06529c4d9
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-27 10:37:40 +0300

    Create if statements.cpp

---
+++ if statements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+int age;
+
+if (age = 18){
+  std:: cout << "welcome to this site";
+}
+else if (age < 0){
+  std:: cout << " you have not been born yet";
+}
+else if (age > 100){
+  std:: cout << "you are too old to enter this site";
+}
+else{
+  std:: cout << "you are not old enough to enter this site";
+}
+
+  return 0;
+}
