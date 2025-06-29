bbc473c2496b201c14249585297d37d7c9bec633
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-29 14:02:41 +0300

    Create for loops.cpp

---
+++ for loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+    for(int i = 1; i <= 3; i++){
+     std::cout << "happy new year!";
+    }
+
+    for(int i = 1; i <= 10; i+= 2){
+     std:: cout << i << '\n';
+    }
+
+    std:: cout << "happy new year!" << '\n';
+    
+    for(int i = 10; i >= 0; i-= 2){
+    std:: cout << i << '\n';
+    }
+    return 0;
+}
