c61384181706be3e51cb74fded0f6ff401a127db
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2025-06-29 14:49:16 +0300

    Create break & continue.cpp

---
+++ break & continue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+
+   for(int i = 0; i <= 20; i++){
+
+        if(i=13){
+            break;
+        }
+    std:: cout << i << '\n';
+   }
+
+   for(int i = 0; i<= 20; i++){
+        
+        if(i==13){
+            continue;
+        }
+    std:: cout << i << '\n';
+ }
+
+
+    return 0;
+}
+
